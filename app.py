dc3fd80b1545563ce60074e0468027a9106ef690
anshumkumar <148078237+anshumkumar@users.noreply.github.com>
2026-04-04 00:15:56 +0100

    (version 1.4)

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
 # file for main flask app.
+
+from flask import Flask, render_template, request, redirect, session
+from database.db import db_connection
+from models.user import User
+from models.ticket import Ticket
+from datetime import datetime
+
+app = Flask(__name__)
+app.secret_key = "akumar_secret_key"
+
+@app.route('/')
+def login():
+    ()
+
+@app.route('/logout')
+def logout():
+    ()
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    ()
